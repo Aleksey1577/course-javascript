d25f2ecc74f373ebe2a44c40adf846b5e52ac380
Aleksey1577 <alekseykras94@gmail.com>
2022-07-04 15:29:49 +0300

    week2.1

--- projects/test/index.js
+++ projects/test/index.js
@@ -6,3 +6,10 @@ const result = document.querySelector('#my-result');
 button.addEventListener('click', () => {
   result.textContent = 42;
 });
+
+
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+};
